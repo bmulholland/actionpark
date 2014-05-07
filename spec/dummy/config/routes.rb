808347d86e3b182c67ced4a5b06fa0550fb59286
Brendan Mulholland <me@bmulholland.ca>
2014-05-06 20:34:48 -0700

    Add routes and remove a spec

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
 
   mount Actionpark::Engine => "/actionpark"
+
+  get "loop_ride/start"
+  get "loop_ride/get_stuck"
 end
--- spec/models/actionpark/request_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-module Actionpark
-  describe Request do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-end
