e49bfbd0a22a7a46d876f73887e7ed15d5ff4490
Florida Elago <florida.elago@gmail.com>
2014-05-06 20:58:49 -0700

    add user model

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,13 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140507031829) do
+ActiveRecord::Schema.define(:version => 20140507035404) do
 
   create_table "actionpark_requests", :force => true do |t|
     t.string   "controller_name"
     t.string   "action_name"
     t.integer  "user_id"
     t.datetime "created_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
