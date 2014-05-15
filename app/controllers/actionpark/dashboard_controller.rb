d71b260a6106903754e23981ede425281ebfea7b
Brendan Mulholland <me@bmulholland.ca>
2014-05-14 20:13:16 -0700

    Add a filter form to the dashboard

--- app/controllers/actionpark/dashboard_controller.rb
+++ app/controllers/actionpark/dashboard_controller.rb
@@ -3,7 +3,11 @@ require_dependency "actionpark/application_controller"
 module Actionpark
   class DashboardController < ApplicationController
     def index
-      @requests = Request.limit(100).all
+      conditions = {}
+
+      conditions[:user_id] = params[:user_id] if params[:user_id]
+
+      @requests = Request.where(conditions).limit(100).all
     end
   end
 end
